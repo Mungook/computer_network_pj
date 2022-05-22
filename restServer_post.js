1055a4ff666021db556c7f4f6644d260582648ad
mungook <qkranstjd01@gmail.com>
2022-05-22 15:25:33 +0900

    finish post

--- restServer_post.js
+++ restServer_post.js
@@ -3,4 +3,16 @@ var queryString = require('querystring');
 
 http.createServer((req,res)=>{
  // Write your own codes
+    if(req.method == 'POST'){
+        req.on('data', function(chunk){
+            //console.log(chunk.toString());
+            var data = queryString.parse(chunk.toString());
+            console.log("name: " + "%s" + "\nnumber: " + "%d",data.name, data.number);
+            res.writeHead(200, {'Content-Type' : 'text/html'});
+            res.end('name : ' + data.name + '\nstudent number : ' + data.number);
+        });
+    }
+}).listen(8080, function(){
+    console.log('server running on 8080.'); 
 });
+
